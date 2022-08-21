f9da647af907fa836fd9b409c024c777be2105e2
dshukertjr <18113850+dshukertjr@users.noreply.github.com>
2022-08-22 00:47:44 +0900

    some

--- lib/main.dart
+++ lib/main.dart
@@ -34,9 +34,12 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  final supabaseUrl = 'https://nlbsnpoablmsxwkdbmer.supabase.co';
+  // final supabaseUrl = 'https://nlbsnpoablmsxwkdbmer.supabase.co';
+  // final supabaseKey =
+  //     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYyOTE5ODEwMiwiZXhwIjoxOTQ0Nzc0MTAyfQ.XZWLzz95pyU9msumQNsZKNBXfyss-g214iTVAwyQLPA';
+  final supabaseUrl = 'https://bbifkfukkjpeiygqrkip.supabase.co';
   final supabaseKey =
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYyOTE5ODEwMiwiZXhwIjoxOTQ0Nzc0MTAyfQ.XZWLzz95pyU9msumQNsZKNBXfyss-g214iTVAwyQLPA';
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJiaWZrZnVra2pwZWl5Z3Fya2lwIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTcwOTk1MTgsImV4cCI6MTk3MjY3NTUxOH0.XGGVrF_EPqSBd7vjABKGxVXqT7s6ntGElNT_oyhHMMY';
   // late final SupabaseClient supabase;
   // @override
   // void initState() {
@@ -53,43 +56,73 @@ class _HomePageState extends State<HomePage> {
   //   super.initState();
   // }
 
+  late final RealtimeChannel channel;
+
+  // Future<void> _getSomething() async {
+  //   await Future.delayed(const Duration(seconds: 5));
+
+  //   final supabase = SupabaseClient(supabaseUrl, supabaseKey);
+  //   final data = await supabase.from('chats').insert({'content': 'sfsad'});
+  //   _getSomething();
+  // }
+
   @override
   void initState() {
     super.initState();
-    // supabase = SupabaseClient(supabaseUrl, supabaseKey);
+
+    // _getSomething();
 
     final client = RealtimeClient(
       'ws://${supabaseUrl.split('://').last}/realtime/v1',
       params: {'apikey': supabaseKey},
       // ignore: avoid_print
       logger: (kind, msg, data) {
-        print('$kind $msg $data');
+        print('within logger $kind $msg $data');
       },
     );
 
-    final channel = client.channel('realtime:public:random');
-    channel.on('DELETE', {}, (payload, [ref]) {
-      print('channel delete payload: $payload');
+    channel = client.channel('some', {
+      'config': {
+        'broadcast': {'ack': true, 'self': true},
+      }
     });
-    channel.on('INSERT', {}, (payload, [ref]) {
-      print('channel insert payload: $payload');
+    // channel.on('DELETE', {}, (payload, [ref]) {
+    //   print('channel delete payload: $payload');
+    // });
+
+    // channel.on(
+    //     'postgres_changes',
+    //     ChannelFilter(
+    //       event: 'INSERT',
+    //       schema: 'public',
+    //       table: 'chats',
+    //       // 'filter': 'id=eq.1'
+    //     ), (payload, [ref]) {
+    //   print('🎉🎉🎉 channel insert payload: $payload');
+    // });
+
+    channel.on('broadcast', ChannelFilter(event: 'location'), (payload, [ref]) {
+      print('🚀🚀🚀 broadcast 🚀');
+      print(payload);
     });
 
-    final presenceChannel = client.channel('something');
+    channel.subscribe();
 
-    presenceChannel.on('broadcast', {'event': 'track'}, (payload, [ref]) {
-      print('$ref, $payload');
-    });
+    // final presenceChannel = client.channel('something');
 
-    // presenceChannel.push('track', {'another': 'yay'});
+    // presenceChannel.on('broadcast', {'event': 'track'}, (payload, [ref]) {
+    //   print('within presence channel $ref, $payload');
+    // });
 
-    client.onMessage((message) {
-      print('MESSAGE $message');
-    });
+    // // presenceChannel.push('track', {'another': 'yay'});
+
+    // client.onMessage((message) {
+    //   print('within on message $message');
+    // });
 
     // on connect and subscribe
-    client.connect();
-    channel.subscribe().receive('ok', (_) => print('SUBSCRIBED'));
+    // client.connect();
+    // channel.subscribe();
   }
 
   @override
@@ -98,18 +131,13 @@ class _HomePageState extends State<HomePage> {
       body: Center(
         child: ElevatedButton(
           onPressed: () async {
-            // try {
-            //   await supabase.rpc('some');
-            //   final res = await supabase.from('products').update(
-            //       {'name': 'Relaxed Fit Twill Pull-on Pants'}).eq('id', 1);
-            //   ScaffoldMessenger.of(context)
-            //       .showSnackBar(const SnackBar(content: Text('Success!')));
-            //   print(res);
-            // } catch (e) {
-            //   print(e);
-            //   ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-            //       backgroundColor: Colors.red, content: Text(e.toString())));
-            // }
+            final status = await channel.send({
+              'type': 'broadcast',
+              'event': 'location',
+              'payload': {'here': 'is this shown?'},
+            });
+            print('status: ');
+            print(status);
           },
           child: const Text('here'),
         ),
